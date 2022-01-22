79e50a14781dff762a647fb7c42a7d9dac382170
Fernando Gonzalez Serrano <nano9gs@hotmail.es>
2022-01-22 19:56:59 +0100

    fixedd

--- database/db_config.js
+++ database/db_config.js
@@ -8,7 +8,7 @@ const config = {
         password: 'd7ed8ff3071eeaad88e3bb6a9b0fa27933bbf1e519e311ef3dd352f9efa944ac',
         database: 'd6c3dv2ucoij2j',
     },
-    listPerPage: env.LIST_PER_PAGE || 10,
+    //listPerPage: env.LIST_PER_PAGE || 10,
     connectionString: 'postgres://blqkxpihnhoiqy:d7ed8ff3071eeaad88e3bb6a9b0fa27933bbf1e519e311ef3dd352f9efa944ac@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d6c3dv2ucoij2j',
     //connectionString: process.env.DATABASE_URL,
     ssl: {
