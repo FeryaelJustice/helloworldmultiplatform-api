9ac932e0cc5acb9f87fad71b6656c01cac98fff5
Fernando Gonzalez Serrano <nano9gs@hotmail.es>
2022-01-22 20:46:27 +0100

    fix ejs

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.get('/players', (req, res) => {
 app.get('/db', async (req, res) => {
     try {
         const client = await pool.connect();
-        const result = await client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null };
+        const results = await client.query('SELECT * FROM test_table');
+        //const results = { 'results': (result) ? result.rows : null };
         res.render('pages/db', {
             results: results
         });
